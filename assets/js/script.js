b79b2dbe3ee06b7eb93ec00f187af77c8409d20b
BrownCAB <brown.calah@hotmail.com>
2022-09-27 16:04:58 -0700

    Generating HTML with Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,95 @@
 // Variables
-var inputValue = $('#inputValue')
-var searchBtn = document.querySelector('#searchBtn');
-var saveSearch = ('#search');
+var inputValue = $("#inputValue");
+var searchBtn = document.querySelector("#searchBtn");
+var saveSearch = "#search";
 
-var cityName = document.querySelector('.cityName');
-var mainDate = '';
-var mainIcon = document.querySelector('mainIcon'); 
-var mainTemp = document.querySelector('.mainTemp');
-var mainWind = document.querySelector('.mainWind');
-var mainHumid = document.querySelector('.mainHumid');
-var mainUv = document.querySelector('.mainUv');
+var cityNameContainer = document.querySelector(".cityName");
+var locationContainer = document.querySelector(".location-container");
 
-var date = ('.date');
-var icon = ('.icon');
-var temp = ('.temp');
-var wind = ('.wind');
-var humid = ('.humid');
- 
+var cardDeck = document.querySelector('card-deck')
 
 //API Varibales
-var APIKey = 'f6862802cdafd2f8859444a3108a7a22'; 
+var APIKey = "f6862802cdafd2f8859444a3108a7a22";
 
+// event listner and fetch for current Forcast
+searchBtn.addEventListener("click", function () {
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    inputValue.val() +
+    "&appid=" +
+    APIKey + "&units=imperial";
 
-
-// event listner and fetch
-searchBtn.addEventListener('click', function() {
-var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + inputValue.val() + '&appid=' + APIKey;
-
-    console.log(queryURL);
-    fetch(queryURL).then(response => response.json())
-    .then(response => console.log(response))
-    .then(data => {
-        cityNameValue = data['name'];
-        mainTempValue = data['main']['temp'];
-
+  console.log(queryURL);
+  fetch(queryURL)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        showCurrentWeather(data);
+        showFiveDay(data.coord.lat, data.coord.lon)
     })
-    
-    .catch(err => alert("Not A City Name!"))
-})
+
+    .catch((err) => {
+     console.log(err)
+    });
+});
+
+function showCurrentWeather(data) {
+    var cityName = document.createElement("div")
+    var date = document.createElement("div")
+    var mainIcon = document.createElement("img")
+    var mainTemp = document.createElement("p")
+    var mainWind = document.createElement("p")
+    var mainHumid = document.createElement("p")
+    var mainUv = document.createElement("p")
+    cityName.textContent = data.name
+    date.textContent = data.dt
+    mainIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    mainTemp.textContent = data.main.temp
+    mainWind.textContent = data.wind.speed
+    mainHumid.textContent = data.main.humidity
+
+    cityNameContainer.append(cityName, date, mainIcon)
+    locationContainer.append(mainTemp, mainWind, mainHumid)
+}
+
+// event listner and fetch for 5-day Forcast
+function showFiveDay(lat, lon) {
+    var queryURL =
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
+
+  console.log(queryURL);
+  fetch(queryURL)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        for (let i = 7; i < data.list.length; i+=7) {
+            var card = document.createElement("div")
+            var date = document.createElement("div")
+            var mainIcon = document.createElement("img")
+            var mainTemp = document.createElement("p")
+            var mainWind = document.createElement("p")
+            var mainHumid = document.createElement("p")
+            
+            date.textContent = data.list[i].dt
+            mainIcon.src = `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`
+            mainTemp.textContent = data.list[i].main.temp
+            mainWind.textContent = data.list[i].wind.speed
+            mainHumid.textContent = data.list[i].main.humidity
+            
+            card.append(date, mainIcon, mainTemp, mainWind, mainHumid)
+            cardDeck.append(card)           
+        }
+        console.log(card)
+    })
+    .catch((err)=> console.log(err))
+}
 
 
-// Search Data is saved to local storage 
 
+// Search Data is saved to local storage
 
+// local storage data is accessed from the local storage
 
-// local storage data is accessed from the local storageg
+// Loads current forcast into the HTML
 
-
-
-// Loads current forcast
-
-
-
-// Loads future 5 Day forcast
-
-
+// Loads future 5 Day forcast into the HTML
